a109f99953156c97cad20623d07554de6e60bda9
Austin DeMordaunt <austin.demordaunt@kellogg.northwestern.edu>
2025-03-15 22:14:59 +0000

    update. next photos

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,42 +1,40 @@
 class EntriesController < ApplicationController
   def index
-    if current_user
-      # Only display entries for the current user.
-      @entries = Entry.where(user_id: current_user["id"])
-    else
-      flash["notice"] = "You must be logged in to view your entries."
-      redirect_to "/login"
+    unless current_user
+      flash[:notice] = "You must be logged in to view entries."
+      redirect_to "/login" and return
     end
+    @entries = Entry.where(user_id: current_user["id"])
   end
 
   def new
-    if current_user
-      @entry = Entry.new
-    else
-      flash["notice"] = "You must be logged in to create a new entry."
-      redirect_to "/login"
+    unless current_user
+      flash[:notice] = "You must be logged in to create a new entry."
+      redirect_to "/login" and return
     end
+    @entry = Entry.new
   end
 
   def create
-    if current_user
-      @entry = Entry.new
-      @entry["title"] = params["title"]
-      @entry["description"] = params["description"]
-      @entry["occurred_on"] = params["occurred_on"]
-      @entry["place_id"] = params["place_id"]
-      # Assign the new entry to the logged-in user.
-      @entry["user_id"] = current_user["id"]
-      if @entry.save
-        flash["notice"] = "Entry created successfully!"
-        redirect_to "/places/#{@entry["place_id"]}"
-      else
-        flash["notice"] = "There was a problem creating your entry."
-        render :new
-      end
+    unless current_user
+      flash[:notice] = "You must be logged in to create an entry."
+      redirect_to "/login" and return
+    end
+
+    @entry = Entry.new
+    @entry["title"] = params["title"]
+    @entry["description"] = params["description"]
+    @entry["occurred_on"] = params["occurred_on"]
+    @entry["place_id"] = params["place_id"]
+    # Assign the entry to the logged-in user.
+    @entry["user_id"] = current_user["id"]
+
+    if @entry.save
+      flash[:notice] = "Entry created successfully!"
+      redirect_to "/places/#{@entry["place_id"]}"
     else
-      flash["notice"] = "You must be logged in to create an entry."
-      redirect_to "/login"
+      flash[:notice] = "There was a problem creating the entry."
+      render :new
     end
   end
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,22 +1,48 @@
 class PlacesController < ApplicationController
-
+  # For the index action, if the user is not logged in, we display an empty list.
   def index
-    @places = Place.all
-  end
-
-  def show
-    @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+    if current_user
+      @places = Place.all
+    else
+      @places = []   # Non-signed in users see no places.
+    end
   end
 
   def new
+    unless current_user
+      flash[:notice] = "You must be logged in to add a new place."
+      redirect_to "/login" and return
+    end
+    @place = Place.new
   end
 
   def create
+    unless current_user
+      flash[:notice] = "You must be logged in to add a new place."
+      redirect_to "/login" and return
+    end
+
     @place = Place.new
     @place["name"] = params["name"]
-    @place.save
-    redirect_to "/places"
+    # Add any additional attributes as needed.
+    if @place.save
+      flash[:notice] = "Place created successfully!"
+      redirect_to "/places"
+    else
+      flash[:notice] = "There was a problem creating the place."
+      render :new
+    end
   end
 
+  def show
+    unless current_user
+      flash[:notice] = "You must be logged in to view this place."
+      redirect_to "/login" and return
+    end
+    @place = Place.find(params["id"])
+    # Using the association to fetch entries that belong to this place and the current user.
+    @entries = @place.entries.where(user_id: current_user["id"])
+  end
+  
+  
 end
